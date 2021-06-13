ed64646fcf94de3622be3303468ed32680142a28
Caroline <haliieva.kr@gmail.com>
2021-06-13 22:31:31 +0300

    Objects

---
+++ 2021-05-28/5.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* 5. Динамически (скриптом) создайте HTML элемент <dl>. Переберите в цикле (for..in) сгенерированный ранее объект person, 
+добавляя dt - для имени свойства и dd - для значения свойства (работа с DOM - с использованием методов Native JS). */
+
+const person = {
+  name: 'Sasha',
+  age: 25,
+  sex: 'male'
+};
+
+let element = document.createElement('div');
+document.body.appendChild(element)
+
+
+let nodeList = document.createElement('dl')
+element.appendChild(nodeList)
+
+
+for (let key in person) {
+  console.log(person[key])
+
+
+  let nodeTitle = document.createElement('dt');
+  nodeList.appendChild(nodeTitle)
+  nodeTitle.innerText = key;
+
+  let nodeDefinition = document.createElement('dd')
+  nodeTitle.appendChild(nodeDefinition)
+  nodeDefinition.innerText = person[key];
+}
